c58719aaed677f879b6ba0d911a14041cb2c987a
Chungyuen Li <lizoyu1@gmail.com>
2016-12-12 09:17:32 -0600

    Exception added

--- priorGrid.py
+++ priorGrid.py
@@ -2,13 +2,28 @@
 # coding = UTF-8
 import numpy as np
 
+class Error(Exception):
+   """Base class for other exceptions"""
+pass
+
+class WrongAnswerError(Error):
+   """Raised when the input value is wrong"""
+pass
+
 newtype = np.dtype({
     'names':['name','value'],
     'formats':[(str,32),'i']
     })
 
-reply = input('How many objects?\n')
-objects = np.zeros((int(reply),), dtype = newtype)
+while True:
+    try:
+        reply = int(input('How many objects? (in numbers)\n'))
+    except ValueError:
+        print('Please give me the number, like 1, 2, 3.')
+        continue
+    break
+
+objects = np.zeros((reply,), dtype = newtype)
 pointer = 0
 
 while True:
@@ -23,11 +38,18 @@ j_count = 1
 while True:
     i = 0
     for j in range(j_count, len(objects)):
-        reply = input('Is [' + objects[i][0] +
+        while True:
+            try:
+                reply = input('Is [' + objects[i][0] +
                       '] more important than [' + objects[j][0] + '] ?\n')
+                if reply != 'y' and reply != 'n':
+                    raise WrongAnswerError
+            except WrongAnswerError:
+                print('You should answer with y (yes) or n (no).')
+                continue
+            break
         if reply == 'y': objects[i]['value'] += 1
-        if reply == 'n': objects[j]['value'] += 1
-        i += 1
+        elif reply == 'n': objects[j]['value'] += 1
     j_count += 1
     if j_count == len(objects): break
 
